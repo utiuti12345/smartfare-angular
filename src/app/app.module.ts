8524bd5e1f3f0832f9a6cda259b821bcf9acd996
Yuta Saotome <private.uta.sao@gmail.com>
2020-04-09 18:07:45 +0900

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { InputComponent } from './pages/input/input.component';
+import { DetailComponent } from './pages/input/detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    DetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/input/detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-component',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
+})
+export class DetailComponent {
+}
